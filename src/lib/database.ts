ede3975eeea15531d268f35ba09bb7387b3a1835
rd1616 <rauldaramus@icloud.com>
2024-01-22 22:40:50 +0100

    using env

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,17 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
+require('dotenv').config()
 
-// URI OF THE MONGOGB (IN THIS CASE MongoDB Atlas)
-const MongoURI = "mongodb+srv://admin:HPvzCfgcWT1K0XuA@cluster.ukkr5pz.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose.set('strictQuery', true);
-
+mongoose.set('strictQuery', true)
 export const connectDB = async (): Promise<void> => {
   await mongoose
-    .connect(MongoURI)
-    .catch((err) => console.error(err));
+    .connect(String(process.env.MONGO_URI))
+    .catch((err) => console.error(err))
 }
-
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 mongoose.connection.on('error', (err) => {
-  console.error(`Mongoose error → ${String(err.message)}`);
-});
+  console.error(`Mongoose error → ${String(err.message)}`)
+})
