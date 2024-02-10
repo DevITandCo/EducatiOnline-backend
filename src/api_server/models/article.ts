dc9c075ad373ab9c7141102dfe4a61f922131abe
p-yerro <113281998+wattbreak@users.noreply.github.com>
2024-02-10 08:01:01 +0100

    removing comments

--- src/api_server/models/article.ts
+++ src/api_server/models/article.ts
@@ -46,21 +46,5 @@ const articleSchema = new Schema<ArticleDocument>(
     timestamps: true
   }
 )
-// Titre
-// Pathologie (terme technique)
-// Symptômes
-// Comment contribuer au bien-être, sécuriser l'apprentissage de la réussite
-// Liste des procédures pour se faire aider à la prise en charge
-// Lien pour du contenu supplémentaire
-// Déficiences liées
-// Formulaire de contact
-
-
-
-/**
- * TODO quand tu reviens de fumer/autre:
- * arranger le code backend en consequence du nouveau modele d'article
- * adapter les codes frontend pour le get article
- */
 
 export const ArticleModel = model<ArticleDocument>('Article', articleSchema)
