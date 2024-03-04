56947e8e32f358db22530ce59e544346642aa3d8
rd1616 <rauldaramus@icloud.com>
2024-03-04 19:54:33 +0100

    Trying to secret the URI

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,13 +1,15 @@
-import mongoose from 'mongoose'
-require('dotenv').config()
+import mongoose from 'mongoose';
+require('dotenv').config();
 
-mongoose.set('strictQuery', true)
+mongoose.set('strictQuery', true);
+
 export const connectDB = async (): Promise<void> => {
   await mongoose
-    .connect("mongodb+srv://admin:HPvzCfgcWT1K0XuA@cluster.ukkr5pz.mongodb.net/?retryWrites=true&w=majority")
-    .catch((err) => console.error(err))
-}
-mongoose.Promise = global.Promise
+    .connect(process.env.MONGODB_URI)
+    .catch((err) => console.error(err));
+};
+
+mongoose.Promise = global.Promise;
 mongoose.connection.on('error', (err) => {
-  console.error(`Mongoose error → ${String(err.message)}`)
-})
+  console.error(`Mongoose error → ${String(err.message)}`);
+});
